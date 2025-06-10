17df7ad892c5a0fc362a9b1e1ce64b0cf54bd670
Blakthorne <dlpolar38@gmail.com>
2025-06-09 23:48:46 -0400

    figured out how to connect rounded borders to create a curved line

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { animate, createScope, Scope } from "animejs";
+import Link from "next/link";
 import { useEffect, useRef } from "react";
 
 export default function Home() {
@@ -26,7 +27,7 @@ export default function Home() {
                 delay: (_, i) => i * 50, // Function based value
                 ease: "inOutCirc",
                 loopDelay: 1000,
-                loop: true
+                loop: false
             });
         });
 
@@ -36,26 +37,48 @@ export default function Home() {
 
     return (
         <div
-            className="flex h-screen flex-col items-start justify-center px-56"
+            className="flex h-screen flex-col items-start justify-between"
             ref={root}
         >
-            <div className="-mt-24 flex bg-gradient-to-r from-blue-600 to-purple-500 bg-clip-text pl-8 pt-24 text-left font-sans text-8xl font-extrabold text-transparent">
-                <div className="span">H</div>
-                <div className="span">I</div>
-                <div className="span">,</div>
-                <div className="span">&nbsp;</div>
-                <div className="span">I</div>
-                <div className="span">'</div>
-                <div className="span">M</div>
-                <div className="span">&nbsp;</div>
-                <div className="span">D</div>
-                <div className="span">A</div>
-                <div className="span">V</div>
-                <div className="span">I</div>
-                <div className="span">D</div>
+            <div className="invisible">
+                --this is a hidden element, you aren't supposed to see this--
             </div>
-            <div className="mt-8 pl-8 text-left font-mono text-xl font-extrabold">
-                Thanks for visiting!
+            <div className="mx-56">
+                <div className="-mt-24 flex bg-gradient-to-r from-blue-600 to-purple-500 bg-clip-text px-56 pl-8 pt-24 text-left font-sans text-8xl font-extrabold text-transparent">
+                    <div className="span">H</div>
+                    <div className="span">I</div>
+                    <div className="span">,</div>
+                    <div className="span">&nbsp;</div>
+                    <div className="span">I</div>
+                    <div className="span">'</div>
+                    <div className="span">M</div>
+                    <div className="span">&nbsp;</div>
+                    <div className="span">D</div>
+                    <div className="span">A</div>
+                    <div className="span">V</div>
+                    <div className="span">I</div>
+                    <div className="span">D</div>
+                </div>
+
+                <div className="mt-8 pl-8 text-left font-mono text-xl font-extrabold">
+                    Thanks for visiting!
+                </div>
+            </div>
+            <div className="mb-4 flex w-screen items-end justify-between">
+                <div className="box-content h-4 w-[35vw] border-b-[1px] bg-slate-600 text-white" />
+                <div className="box-content h-4 w-[5vw] rounded-br-full border-b-[1px] border-r-[1px] bg-slate-600 text-white" />
+                <div className="-ml-[0.6px] mb-4 box-content h-4 w-[5vw] rounded-tl-full border-l-[1px] border-t-[1px] text-white" />
+                <div className="box-border h-4 w-[10vw] border-t-[1px] pb-8 text-white">
+                    <Link
+                        className="mt-4 text-center font-mono text-white"
+                        href="#"
+                    >
+                        read more about me
+                    </Link>
+                </div>
+                <div className="-mr-[0.6px] mb-4 box-content h-4 w-[5vw] rounded-tr-full border-r-[1px] border-t-[1px] text-white" />
+                <div className="box-content h-4 w-[5vw] rounded-bl-full border-b-[1px] border-l-[1px] bg-slate-600 text-white" />
+                <div className="box-content h-4 w-[35vw] border-b-[1px] bg-slate-600 text-white" />
             </div>
         </div>
     );
